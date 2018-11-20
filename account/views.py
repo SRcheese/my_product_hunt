b85f9d80e9de810d1ed41a60c5c12544f6f53c3e
Unknown <2631963011@qq.com>
2018-11-20 10:29:20 +0800

    添加了登录和退出功能

--- account/views.py
+++ account/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render,redirect
 from django.contrib.auth.models import User
+from django.contrib import auth
 
 # Create your views here.
 
@@ -23,3 +24,22 @@ def signup(request):
 				return redirect('主页')
 			else:
 				return render(request,'signup.html',{'密码错误':'两次输入的密码不一致'})
+
+
+def login(request):
+	if request.method=='GET':
+		return render(request,'login.html')
+	elif request.method=='POST':
+		user_name=request.POST['UserName']
+		pass_word=request.POST['Password']
+		user=auth.authenticate(username=user_name,password=pass_word)
+		if user is None:
+			return render(request,'login.html',{'登录错误':'用户名或密码错误'})
+		else:
+			auth.login(request,user)
+			return redirect('主页')
+
+def logout(request):
+	if request.method=='POST':
+		auth.logout(request)
+		return redirect('主页')
